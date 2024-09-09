896eed9d446fdada7c6ee6b8ccdd4fbf630b7764
Kieran <kieran.atkins99@gmail.com>
2024-09-09 11:28:20 +0100

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export images=./images/*.png
+export images=./test_images/*.png
 export weight=./epoch_36.pth
 
 # Generate outputs for phenotyping
-export out=./outputs/
-python inference.py inference "$cfg_path" "$weight" "$images" "$out"
+# export out=./outputs/
+# python inference.py inference "$cfg_path" "$weight" "$images" "$out"
 
 # Generate images
 export out=./visualisations/
