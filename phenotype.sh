8d311f3acbe383622feae7010a386f8c64ea134f
Kieran <kieran.atkins99@gmail.com>
2024-09-27 16:36:08 +0100

    Update phenotype.sh

--- phenotype.sh
+++ phenotype.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-out=./out/preds
+data=./out/preds
 
 # For running with MPI on slurm
 # mpiexec -n $SLURM_NTASKS python -m mpi4py.futures phenotype_slurm.py "$out" --scale=1.0
 
 # Running with python concurrent library
-python phenotype.py "$out" --scale=1.0
+python phenotype.py "$data" --scale=1.0
 
 
